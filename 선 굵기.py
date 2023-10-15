3d6afd4da096339ba6f02f7f5a7e92bd8f10b817
nicholasbear <nicholasbear@naver.com>
2023-10-15 17:11:23 +0900

    최적화

--- 선 굵기.py
+++ 선 굵기.py
@@ -5,34 +5,27 @@ import numpy as np
 img_array = np.fromfile(r'C:\Users\nicho\Desktop\hackathon_data\Data\House_train\images\집_7_남_03613.jpg', np.uint8)
 image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
+# 이미지를 그레이 스케일로 변환합니다.
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# threshold보다 큰 값을 가진 픽셀을 흰색으로 변경합니다.
+binary_image = np.where(gray_image > 220, 255, gray_image)
+
+# 전체 픽셀수
+binary_pixel_count = np.count_nonzero(binary_image != 255)
+
 # Canny 엣지 검출
-canny = cv2.Canny(image, 100, 200)  # 100과 200은 경계값(threshold)으로 조정 가능합니다.
+canny = cv2.Canny(binary_image, 100, 200)  # 100과 200은 경계값(threshold)으로 조정 가능합니다.
+
+# 결과 이미지를 보여주기
+cv2.imshow("Canny Edge Detection", canny)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # 경계선 픽셀 수 계산
 edge_pixel_count = np.count_nonzero(canny)
 
 # 경계선 픽셀 수 출력
 print(f'경계선의 픽셀 수: {edge_pixel_count}')
-
-def count_dark_pixels(image, threshold=200):
-    # 어두운 픽셀 추출
-    dark_pixels = (image < threshold).astype(np.uint8) * 255  # 어두운 픽셀을 흰색(255)으로, 나머지를 검은색(0)으로 설정
-
-   # 어두운 픽셀 수 계산
-    dark_pixel_count = (dark_pixels == 255).sum()
-
-    return dark_pixels, dark_pixel_count
-
-# 이미지를 불러옵니다.
-img_array = np.fromfile(r'C:\Users\nicho\Desktop\hackathon_data\Data\House_train\images\집_7_남_03613.jpg', np.uint8)
-image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
-# 어두운 픽셀 추출 및 수 계산 (기본 임계값은 128)
-dark_pixels, dark_pixel_count = count_dark_pixels(image)
-
-# 어두운 부분 이미지를 화면에 표시
-cv2.imshow('Dark Pixels', dark_pixels)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-print(f'어두운 픽셀 수: {dark_pixel_count}')
+print(f'전체 픽셀 수: {binary_pixel_count}')
+print(f'비율: {edge_pixel_count/binary_pixel_count}')
--- 지면선.py
+++ 지면선.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # 이미지를 불러옵니다.
-img_array = np.fromfile(r'C:\Users\nicho\Desktop\hackathon_data\Data\House_train\images\집_7_남_06076.jpg', np.uint8)
+img_array = np.fromfile(r'C:\Users\nicho\Desktop\hackathon_data\Data\House_train\images\집_7_남_02915.jpg', np.uint8)
 image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 # 이미지의 높이와 너비를 얻습니다.
 height, width, _ = image.shape
@@ -15,16 +15,13 @@ image[:top_height, :] = (255, 255, 255)  # 상단 영역을 흰색으로 설정
 image[-bottom_height:, :] = (255, 255, 255)  # 하단 영역을 흰색으로 설정
 
 # 이미지를 그레이스케일로 변환합니다.
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# 가우시안 블러로 노이즈를 제거합니다.
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-
-# Canny 엣지 검출을 사용하여 에지를 찾습니다.
-edges = cv2.Canny(blurred, 50, 150)
+# threshold보다 큰 값을 가진 픽셀을 흰색으로 변경합니다.
+binary_image = np.where(gray_image > 220, 255, gray_image)
 
 # 에지 이미지에서 선을 찾습니다. maxlineGap 수정필요
-lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=100)
+lines = cv2.HoughLinesP(binary_image, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=20)
 
 n_line = []
 # 찾은 선을 원본 이미지에 그립니다.
@@ -32,9 +29,9 @@ for line in lines:
     x1, y1, x2, y2 = line[0]
     # 직선의 각도를 계산하고 수평으로 된 직선만을 선택합니다.
     angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
-    if angle >= -20 and angle <= 20:
+    if -40<= angle <= 40:
         n_line.append(line)
-        #cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
 
 # 가장 긴 선분을 찾습니다.
