39d8056ed7e71f3ab2becd64db34b448f71649a3
WooyoungPark <wooyoung9654@gmail.com>
2023-10-24 22:27:04 +0900

    NLP

---
+++ NLP/summarization.py
@@ -0,0 +1,25 @@
+# https://huggingface.co/models
+
+import pandas as pd
+import nltk
+nltk.download('punkt')
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+df = pd.read_csv("C:/Users/user/Desktop/house.csv")
+df.columns = ['exp', 'analysis']
+combined_string = '\n'.join(df['analysis'].astype(str))
+ARTICLE = f'"""\n{combined_string}"""'
+
+model = AutoModelForSeq2SeqLM.from_pretrained('eenzeenee/t5-base-korean-summarization')
+tokenizer = AutoTokenizer.from_pretrained('eenzeenee/t5-base-korean-summarization')
+
+prefix = "summarize: "
+inputs = [prefix + ARTICLE]
+
+
+inputs = tokenizer(inputs, max_length=2000, truncation=True, return_tensors="pt")
+output = model.generate(**inputs, num_beams=3, do_sample=True, min_length=300, max_length=500)
+decoded_output = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
+result = nltk.sent_tokenize(decoded_output.strip())[0]
+
+print('RESULT >>', result)
